691fce0cce5391acca084c2169c69ee5c69962c5
ManzilG <manzilgajurel80@gmail.com>
2017-05-01 15:09:43 -0500

    Adding category function done...

--- src/main/java/com/mum/pm/inventory_module/controller/CategoryController.java
+++ src/main/java/com/mum/pm/inventory_module/controller/CategoryController.java
@@ -3,34 +3,66 @@ package com.mum.pm.inventory_module.controller;
 import com.mum.pm.inventory_module.model.Category;
 import com.mum.pm.inventory_module.model.SubCategory;
 import com.mum.pm.inventory_module.service.CategoryService;
+import com.mum.pm.user_module.model.Student;
+import com.mum.pm.user_module.model.User;
+import com.mum.pm.user_module.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by manzil on 4/25/2017.
  */
 @Controller
-@RequestMapping("/category")
+//@RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    UserService userService;
 
-    @GetMapping("/addCategory")
-    public String categoryForm(Model model) {
-        model.addAttribute("category",new Category());
-        return "category";
+    @RequestMapping(value="/admin/add-category",  method = RequestMethod.GET)
+    public ModelAndView addNewCategory() {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", user.getName() + " " + user.getLastName());
+        modelAndView.addObject("category", new Category());
+        modelAndView.addObject("userRole", "Admin");
+
+        modelAndView.setViewName("add-category");
+        return modelAndView;
     }
 
-    @PostMapping("/addCategory")
-    public void categorySubmit(@ModelAttribute Category category) {
-        categoryService.saveCategory(category);
+    @RequestMapping(value="/admin/add-category",  method = RequestMethod.POST)
+    public ModelAndView createNewCategory(Category category, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+
+
+        modelAndView.addObject("userName",   user.getName() + " " + user.getLastName());
+        modelAndView.addObject("userRole",   "Admin");
+
+        if(categoryService.getCategoryByName(category.getCategoryName()) != null){
+            modelAndView.addObject("successMessage", "This category is already in the system");
+            modelAndView.addObject("category",category);
+        }else{
+            categoryService.saveCategory(category);
+            modelAndView.addObject("category", new Category());
+            modelAndView.addObject("successMessage", "Category added successfully");
+        }
+        modelAndView.setViewName("add-category");
+        return modelAndView;
     }
-
-
 }
+
+
+
+
--- src/main/java/com/mum/pm/inventory_module/dao/SubCategoryDAO.java
+++ src/main/java/com/mum/pm/inventory_module/dao/SubCategoryDAO.java
@@ -8,12 +8,15 @@ import com.mum.pm.inventory_module.model.SubCategory;
         import org.springframework.data.repository.CrudRepository;
         import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
- * Created by govin on 4/27/2017.
+ * Created by manzil on 4/27/2017.
  */
 
 @Repository("subCategoryDAO")
 public interface SubCategoryDAO extends CrudRepository<SubCategory,Integer>{
     public Iterable<SubCategory> findByCategory(Category category);
+    public List<SubCategory> getSubCategoriesByCategory_CategoryId(int id);
 
 }
