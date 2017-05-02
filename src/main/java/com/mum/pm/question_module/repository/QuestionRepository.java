745624668abae2cc455bb5c88df644feb9684505
ManzilG <manzilgajurel80@gmail.com>
2017-05-02 15:27:51 -0500

    Some Changes Today

--- src/main/java/com/mum/pm/question_module/repository/QuestionRepository.java
+++ src/main/java/com/mum/pm/question_module/repository/QuestionRepository.java
@@ -2,9 +2,12 @@ package com.mum.pm.question_module.repository;
 
 import com.mum.pm.question_module.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository("questionRepository")
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+import java.util.List;
 
+@Repository("questionRepository")
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    public List<Question> getAllBySub_category_id(int subCategoryId);
 }
