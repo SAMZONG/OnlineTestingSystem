08447136cd9ee7756f92c7773bca0e6423fbde9e
yaoguai9527 <chuang.huang12@gmail.com>
2017-05-03 21:37:36 -0500

    Datatable and button

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -1,14 +1,23 @@
 $(document).ready( function () {
 	 var table = $('#employeesTable').DataTable({
-			"sAjaxSource": "/student/getAllStudent",
-			"sAjaxDataProp": "",
+			"ajax": "/student/getAllStudent",
+            "sAjaxDataProp":"",
 			"order": [[ 0, "asc" ]],
-			"aoColumns": [
-			      { "mData": "studentId"},
-		          { "mData": "firstName" },
-				  { "mData": "lastName" },
-				  { "mData": "email" },
-				  // { "mData": "active" }
-			]
-	 })
+         	"aoColumns" : [
+			      { "data": "studentId"},
+		          { "data": "firstName" },
+				  { "data": "lastName" },
+				  { "data": "email" },
+				  {
+				  	"target": -1,
+                    "data": null,
+                    "defaultContent": "<button>Click!</button>"
+				  }
+			],
+	 	});
+
+    $('#employeesTable tbody').on( 'click', 'button', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        alert( data["studentId"] +"'s name is: "+ data["email"] );
+    });
 });
