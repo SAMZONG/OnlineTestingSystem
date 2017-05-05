cb34dd6f9e1bc889e080cebab763d78d7d771f36
Awad Odeh <awadodeh@gmail.com>
2017-05-05 14:34:48 -0500

    Access-Key: adding validation and connecting access key to the categories

--- src/main/java/com/mum/pm/user_module/controller/StudentController.java
+++ src/main/java/com/mum/pm/user_module/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.mum.pm.user_module.controller;
 
 import com.mum.pm.user_module.model.StudentAccessKeyForm;
+import com.mum.pm.user_module.model.TestKey;
 import com.mum.pm.user_module.model.User;
 import com.mum.pm.user_module.service.StudentService;
 import com.mum.pm.user_module.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,7 +23,7 @@ import javax.validation.Valid;
 public class StudentController {
 
     @Autowired
-    private UserService userService;
+    private StudentService studentService;
 
     @RequestMapping(value={"/student","/student/test"},  method = RequestMethod.GET)
     public ModelAndView addAccessKey() {
@@ -34,16 +36,20 @@ public class StudentController {
     @RequestMapping(value={"/student/test"},  method = RequestMethod.POST)
     public ModelAndView postAccessKey(@Valid StudentAccessKeyForm studentAccessKeyForm, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
+        String accessKey = studentAccessKeyForm.getAccessKey();
 
-        System.out.println("StudentController.postAccessKey");
+        if(accessKey.isEmpty()){
 
-//        User userExists = userService.findUserByEmail(studentAccessKeyForm.getEmail());
-//        if (userExists != null) {
-//            bindingResult
-//                    .rejectValue("email", "error.user",
-//                            "There is already a user registered with the email provided");
-//        }
-        modelAndView.setViewName("add-access-success");
+            modelAndView.setViewName("add-access-key");
+            return modelAndView;
+        }else if (studentService.isAccessKey(accessKey)){
+            studentService.deactivateAccessKey(accessKey);
+            modelAndView.setViewName("category-subCategory");
+        }else{
+            modelAndView.setViewName("add-access-key");
+            return modelAndView;
+        }
+
         return modelAndView;
     }
 }
--- src/main/java/com/mum/pm/user_module/service/StudentServiceImpl.java
+++ src/main/java/com/mum/pm/user_module/service/StudentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.mum.pm.user_module.service;
 
+import com.mum.pm.user_module.model.TestKey;
+import com.mum.pm.user_module.repository.TestKeyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,4 +13,24 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl implements StudentService{
 
 
+    @Autowired
+    private TestKeyRepository testKeyRepository;
+    @Override
+    public boolean isAccessKey(String accessKey) {
+        TestKey testKey = testKeyRepository.findByTestkeyValue(accessKey);
+       if( testKey!=null && testKey.getTestkeyValue().equals(accessKey) ){
+           if(testKey.getActive() == 1){
+               return true;
+           }
+       }
+       return false;
+
+    }
+
+    @Override
+    public void deactivateAccessKey(String accessKey) {
+        TestKey testKey = testKeyRepository.findByTestkeyValue(accessKey);
+        testKey.setActive(0);
+        testKeyRepository.saveAndFlush(testKey);
+    }
 }
