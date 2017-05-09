c27207f57b692db4df5df1f3525fd3bf44c6e27e
ManzilG <manzilgajurel80@gmail.com>
2017-05-09 18:07:59 -0500

    Report Class

---
+++ src/main/java/com/mum/pm/quiz/controller/ReportController.java
@@ -0,0 +1,17 @@
+package com.mum.pm.quiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by manzi on 5/9/2017.
+ */
+@Controller
+public class ReportController {
+
+    @RequestMapping(value="/admin/reports", method = RequestMethod.GET)
+    public String showReports(){
+        return "report";
+    }
+}
